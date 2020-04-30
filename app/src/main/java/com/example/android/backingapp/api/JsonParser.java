0de575a6fb6601d42c3912536439382ae3621c58
Katarzyna Skocka <katarzyna.skocka@vimn.com>
2020-04-30 08:25:59 +0200

    Add pojos

---
+++ app/src/main/java/com/example/android/backingapp/api/JsonParser.java
@@ -0,0 +1,7 @@
+package com.example.android.backingapp.api;
+
+public class JsonParser {
+
+    private static final String RECIPES_BASE_URI = "https://d17h27t6h515a5.cloudfront.net/";
+    public static final String RECIPES_PATH = "topher/2017/May/59121517_baking/baking.json";
+}
