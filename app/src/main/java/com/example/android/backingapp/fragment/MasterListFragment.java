65722c234a4d9e9ac83a820eda99af321484b9f0
Katarzyna Skocka <katarzyna.skocka@vimn.com>
2020-05-01 12:17:47 +0200

    Add fragment clasess and layouts

---
+++ app/src/main/java/com/example/android/backingapp/fragment/MasterListFragment.java
@@ -0,0 +1,16 @@
+package com.example.android.backingapp.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android.backingapp.R;
+
+public class MasterListFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_master_list);
+    }
+}
---
+++ app/src/main/java/com/example/android/backingapp/fragment/StepDetailsFragment.java
@@ -0,0 +1,16 @@
+package com.example.android.backingapp.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android.backingapp.R;
+
+public class StepDetailsFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_step_details);
+    }
+}
