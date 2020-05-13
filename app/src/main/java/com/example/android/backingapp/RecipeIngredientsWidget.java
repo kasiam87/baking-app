cc8816507e14773dc504631e7b4db895e12ee930
Katarzyna Skocka <katarzyna.skocka@vimn.com>
2020-05-13 17:52:47 +0200

    Update widget

--- app/src/main/java/com/example/android/backingapp/RecipeIngredientsWidget.java
+++ app/src/main/java/com/example/android/backingapp/RecipeIngredientsWidget.java
@@ -1,8 +1,10 @@
 package com.example.android.backingapp;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 /**
@@ -13,18 +15,21 @@ public class RecipeIngredientsWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_ingredients_widget);
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
 //        views.setTextViewText(R.id.recipe_image_widget, widgetText);
 
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.recipe_image_widget, pendingIntent);
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
